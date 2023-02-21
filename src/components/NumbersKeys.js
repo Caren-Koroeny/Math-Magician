f72d9bd823cfd25e51435b623716d5ba63f0e0a6
Caren-Koroeny <carensiya5@gmail.com>
2023-02-21 11:31:55 +0300

    add numberic

--- src/components/NumbersKeys.js
+++ src/components/NumbersKeys.js
@@ -13,7 +13,19 @@ class NumbersKeys extends Component {
 
   render() {
     return (
-      <div />
+      <div className="allNumbers">
+        <button type="button" className="btn btn-primary">7</button>
+        <button type="button" className="btn btn-primary">8</button>
+        <button type="button" className="btn btn-primary">9</button>
+        <button type="button" className="btn btn-primary">4</button>
+        <button type="button" className="btn btn-primary">5</button>
+        <button type="button" className="btn btn-primary">6</button>
+        <button type="button" className="btn btn-primary">1</button>
+        <button type="button" className="btn btn-primary">2</button>
+        <button type="button" className="btn btn-primary">3</button>
+        <button type="button" className="btn btn-primary col-span-2">0</button>
+        <button type="button" className="btn btn-primary">.</button>
+      </div>
     );
   }
 }
