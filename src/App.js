e82e9c2225e8a81a71e376fe174b230030bdf240
Caren-Koroeny <carensiya5@gmail.com>
2023-02-28 17:25:44 +0300

    nav bar working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quotes';
+import NavBar from './pages/NavBar';
 
-function App() {
-  return (
-    <div className="calculatorMain-container">
-      <Calculator />
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <NavBar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/calculator" exact element={<Calculator />} />
+      <Route path="/quote" exact element={<Quote />} />
+
+    </Routes>
+  </>
+);
 
 export default App;
